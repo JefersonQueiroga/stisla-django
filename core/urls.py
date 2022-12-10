cadf1ff4d1796bc6b023bec58ebc6919d6b162b0
Jeferson Queiroga <jefersonqueiroga@gmail.com>
2022-12-10 10:09:01 -0300

    Adicionar login.

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
+from app.views import logout_aplicacao
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls'))
+    path('', include('app.urls')),
+    path('accounts/login/',LoginView.as_view(template_name='login.html'),name='login'),
+    path('logout/', logout_aplicacao ,name='logout')
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
